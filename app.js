c40be689d43c675049a7ceda69981ae40af7d50a
Jessie Yang <jessieyang@Jessies-MacBook-Air.local>
2017-09-26 19:08:21 -0400

    add eventlinstener on roll dice button and show img according to the number generated

--- app.js
+++ app.js
@@ -8,3 +8,54 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+
+var scores, roundScore, activePlayer;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0 
+;
+
+
+
+// document.querySelector('#current-' + activePlayer).textContent = dice;
+
+// var x = document.querySelector('#score-0').textContent;
+// console.log(x);
+
+document.querySelector('.dice').style.display = 'none';
+
+document.querySelector('.btn-roll').addEventListener('click',function(){
+
+	// 1. random number
+    var dice = Math.floor(Math.random()*6) + 1;
+
+	// 2. display the result
+	var diceDOM = document.querySelector('.dice')
+	diceDOM.style.display = 'block';
+	diceDOM.src = 'dice-' + dice + '.png';
+
+
+	// 3.update the round score IF the rolled number was NOT a 1 
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
